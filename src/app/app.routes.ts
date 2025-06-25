e0be42b8609499c9967281a0a4714089094beb91
JoaquinPuchuri <jvacotunjar@gmail.com>
2025-06-25 11:46:58 -0500

    feat(rutas): Se configuro las rutas finales de la app, siendo /productos la ruta principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ProductoLista } from './producto-lista/producto-lista';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'productos', component: ProductoLista }, //redefinimos la ruta de nuestra app
+    { path: '', redirectTo: 'productos', pathMatch: 'full' }, //redireccionamos a productos si no se especifica una ruta
+];
