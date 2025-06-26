f358ea7a78f9882249a7c8c57ec593c8179495dc
JoaquinPuchuri <jvacotunjar@gmail.com>
2025-06-26 08:14:51 -0500

    feat(agregar-producto): Se importan FormsModule, clase Producto, clase ProductoService y Router, necesario para implentar la logica detras de añadir un producto

--- src/app/agregar-producto/agregar-producto.ts
+++ src/app/agregar-producto/agregar-producto.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Producto } from '../producto.model';
+import { ProductoService } from '../producto.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-producto',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './agregar-producto.html',
   styleUrl: './agregar-producto.css'
 })
 export class AgregarProducto {
+  producto: Producto = new Producto();
 
+  private productoServicio = inject(ProductoService);
+  private enrutador = inject(Router);
 }
