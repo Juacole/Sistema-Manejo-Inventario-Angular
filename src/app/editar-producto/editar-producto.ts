d1ce516b667138aed11a8c9528963be4677f741f
JoaquinPuchuri <jvacotunjar@gmail.com>
2025-06-27 11:01:44 -0500

    feat(component): Se añade logica para recuperar el producto  a traves del boton de editar, el cual sera mostrado en un formulario para poder modificarlo

--- src/app/editar-producto/editar-producto.ts
+++ src/app/editar-producto/editar-producto.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Producto } from '../producto.model';
+import { ProductoService } from '../producto.service';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-producto',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './editar-producto.html',
   styleUrl: './editar-producto.css'
 })
 export class EditarProducto {
+  producto: Producto = new Producto();
+  id!: number;
+
+  private productoServicio = inject(ProductoService);
+  private ruta = inject(ActivatedRoute);
+
+  ngOnInit() {
+    this.id = this.ruta.snapshot.params['id'];
+    this.productoServicio.obtenerProductoPorId(this.id).subscribe({
+      next: (data) => {
+        this.producto = data;
+      },
+      error: (error: any) => {
+        console.error('Error al obtener el producto:', error);
+      }
+    });
+  }
+
+
 
 }
